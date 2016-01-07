008edccbf78668ab21ed4b3dab34a1949b98816d
Leland Batey <lelandbatey@gmail.com>
2016-01-06 17:33:14 -0800

    Rename misnamed files

--- startup_whereisleland.sh
+++ startup_whereisleland.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 ### BEGIN INIT INFO
-# Provides:          xwl
+# Provides:          <NAME>
 # Required-Start:    $local_fs $network $named $time $syslog
 # Required-Stop:     $local_fs $network $named $time $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Description:       Xwl flask webapp
+# Description:       <DESCRIPTION>
 ### END INIT INFO
 # The above is boilerplate I don't understand
 
@@ -13,30 +13,38 @@
 SCRIPT="/home/nacro/launchscripts/launch_whereisleland.sh"
 RUNAS="nacro"
 
-PIDFILE=/var/run/xwl.pid
-# LOGFILE=/var/log/xwl.log
+# PIDFILE=/var/run/whereisleland.pid
+# LOGFILE=/var/log/whereisleland.log
 
 start() {
-	if [ -f "$PIDFILE" ] && kill -0 $(cat "$PIDFILE"); then
-		echo 'Service already running' >&2
-		return 1
-	fi
 	echo 'Starting service…' >&2
-	su -c "$SCRIPT" $RUNAS > "$PIDFILE" &
+	su -c "$SCRIPT" $RUNAS &
 	echo 'Service started' >&2
 }
 
 stop() {
-	if [ ! -f "$PIDFILE" ] || ! kill -0 $(cat "$PIDFILE"); then
-		echo 'Service not running' >&2
-		return 1
-	fi
-	echo 'Stopping service...' >&2
-	pkill -P $(pgrep "$SCRIPT") && rm -f "$PIDFILE"
-	echo 'Service stopped' >&2
+	# if [ ! -f "$PIDFILE" ] || ! kill -0 $(cat "$PIDFILE"); then
+	# 	echo 'Service not running' >&2
+	# 	return 1
+	# fi
+	# echo 'Stopping service…' >&2
+	# echo $(ps ax | grep "$SCRIPT" | head -n1 | cut -d' ' -f 1) >&2
+	# kill -15 $(ps ax | grep "$SCRIPT" | head -n1 | cut -d' ' -f 1) #&& rm -f "$PIDFILE"
+	# echo 'Service stopped' >&2
+	echo 'Stopping of this service is not implemented. Please kill manually.'>&2
 }
 
 uninstall() {
+	# echo -n "Are you really sure you want to uninstall this service? That cannot be undone. [yes|No] "
+	# local SURE
+	# read SURE
+	# if [ "$SURE" = "yes" ]; then
+	# 	stop
+	# 	rm -f "$PIDFILE"
+	# 	echo "Notice: log file is not be removed: '$LOGFILE'" >&2
+	# 	update-rc.d -f <NAME> remove
+	# 	rm -fv "$0"
+	# fi
 	echo "Uninstallation is not implemented."
 	echo "You can manually remove this auto-start script using the command 'update-rc.d -f <thisScript> remove"
 }
