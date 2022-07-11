6f4cee704aadcfb24cd9a04fe899e3f2b17f23fa
Leland Batey <lelandbatey@lelandbatey.com>
2022-07-11 16:49:59 -0700

    Update to be systemd compatible

--- install_init-scripts.sh
+++ install_init-scripts.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-LAUNCH_SCRIPTS="/home/leland/projects/launchscripts/"
-INIT_SCRIPTS="/etc/init.d/"
+LAUNCH_SCRIPTS="/home/leland/projects/launchscripts"
+INIT_SCRIPTS="/etc/init.d"
 
 #sudo cp "$LAUNCH_SCRIPTS""startup_xwl.sh" "$INIT_SCRIPTS""startup_xwl"
-sudo cp "$LAUNCH_SCRIPTS""startup_whereisleland.sh" "$INIT_SCRIPTS""startup_whereisleland"
+sudo cp "${LAUNCH_SCRIPTS}/startup_whereisleland.sh" "${INIT_SCRIPTS}/startup_whereisleland"
+sudo cp "${LAUNCH_SCRIPTS}/startup_whereisleland.service" "/etc/systemd/system/"
 
 #update-rc.d startup_xwl defaults
 update-rc.d startup_whereisleland defaults
+sudo systemctl daemon-reload
+sudo systemctl enable startup_whereisleland
+sudo service startup_whereisleland start
